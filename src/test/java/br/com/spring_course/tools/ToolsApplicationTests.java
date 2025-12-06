995586bf3059cfa7fc99a1f2026e083ac381189e
Fabio Machado <fabio.a.machado@hotmail.com>
2025-12-06 11:37:25 -0300

    chore: config inicial do projeto

---
+++ src/test/java/br/com/spring_course/tools/ToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring_course.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
